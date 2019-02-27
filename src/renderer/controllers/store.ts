6c6e83c1d0642df7ba31eb2af3272e34e3ecc989
Ryan Burnett <rdjburnett@gmail.com>
2019-02-27 11:26:48 -0800

    Add attributes to store, saved data.

--- src/renderer/controllers/store.ts
+++ src/renderer/controllers/store.ts
@@ -4,17 +4,21 @@ import * as fs from 'fs';
 
 import { observable } from 'mobx';
 import Profile from './profile/profile';
+import Attributes from './attributes/attributes';
 
 export default class Store {
     @observable profile: Profile;
+    @observable attributes: Attributes;
 
     constructor() {
         this.profile = new Profile();
+        this.attributes = new Attributes();
     }
 
     saveCharacter() {
         const characterData = {
-            profile: this.profile
+            profile: this.profile,
+            attributes: this.attributes
         };
 
         fs.writeFile(
@@ -31,5 +35,6 @@ export default class Store {
         const loadedData = fs.readFileSync('./saves/character.json');
         const parsedData = JSON.parse(loadedData.toString());
         this.profile = parsedData.profile;
+        this.attributes = parsedData.attributes;
     }
 }
