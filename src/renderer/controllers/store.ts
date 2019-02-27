b19720fdacc41f7dd5a74b8ef5b39a92d1891966
Ryan Burnett <rdjburnett@gmail.com>
2019-02-27 09:59:22 -0800

    Initial (rudimentary, file-hardcoded) saving and loading support.

--- src/renderer/controllers/store.ts
+++ src/renderer/controllers/store.ts
@@ -1,5 +1,6 @@
 // import * as C from '@shared/const';
 // import * as D from '@shared/debug';
+import * as fs from 'fs';
 
 import { observable } from 'mobx';
 import Profile from './profile/profile';
@@ -9,5 +10,26 @@ export default class Store {
 
     constructor() {
         this.profile = new Profile();
+    }
+
+    saveCharacter() {
+        const characterData = {
+            profile: this.profile
+        };
+
+        fs.writeFile(
+            './saves/character.json',
+            JSON.stringify(characterData),
+            (err) => {
+                if (err) {
+                    console.error(err);
+                }
+            });
+    }
+
+    loadCharacter() {
+        const loadedData = fs.readFileSync('./saves/character.json');
+        const parsedData = JSON.parse(loadedData.toString());
+        this.profile = parsedData.profile;
     }
   }
